029154d5a23e739cf225eb38719f843d477aff5d
Pankaj K Nayak <pankajtutu1997@gmail.com>
2025-08-02 22:01:22 +0530

    This is the solution to question 2

---
+++ Question2.py
@@ -0,0 +1,6 @@
+list1=[i for i in range(1,11,1)]
+print('original list:',list1)
+list2 = list1[:5]
+print('Extracted first five elements:',list2)
+list3=list2[::-1]
+print('Reversed extracted elements:',list3)
